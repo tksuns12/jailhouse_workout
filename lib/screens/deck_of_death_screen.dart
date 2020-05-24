b2a0307257f5d0cf9617831431d21f08c92e07ba
tksuns12 <tksuns@gmail.com>
2020-05-24 19:14:39 +0900

    deckofdeathscreen 리팩토링

--- lib/screens/deck_of_death_screen.dart
+++ lib/screens/deck_of_death_screen.dart
@@ -12,72 +12,73 @@ class DeckOfDeathScreen extends StatelessWidget {
             deck.timer.cancel();
             return Future(() => true);
           },
-          child: deck.isDone
-              ? deck.isFailed?Material(
-                              child: (Container(
-                            color: Colors.black,
-                            child: Center(
-                                child: Text(
-                              'Time is Up :(\n Try Next Time!',
-                              style: TextStyle(color: Colors.greenAccent, fontSize: 70),
-                            )),
+          child: Material(
+            child: SafeArea(
+              child: deck.isDone
+                  ? deck.isFailed
+                      ? (Container(
+                          color: Colors.black,
+                          child: Center(
+                              child: Text(
+                            'Time is Up :(\n Try Next Time!',
+                            style: TextStyle(
+                                color: Colors.greenAccent, fontSize: 70),
                           )),
-              ):Material(
-                              child: (Container(
-                    color: Colors.black,
-                    child: Center(
-                        child: Text(
-                      'Well Done!',
-                      style: TextStyle(color: Colors.greenAccent, fontSize: 70),
-                    )),
-                  )),
-              )
-              : Material(
-                  color: Colors.black,
-                  child: SafeArea(
-                      child: Container(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text(
-                          '${deck.displayedMin.toString().padLeft(2, '0')}:${deck.displayedSec.toString().padLeft(2, '0')}',
-                          style: TextStyle(
-                              fontSize: 40, color: Colors.greenAccent),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(20),
-                          child: Image.asset(
-                            deck.displayedCard.imagePath,
-                            scale: 2.0,
+                        ))
+                      : (Container(
+                          color: Colors.black,
+                          child: Center(
+                              child: Text(
+                            'Well Done!',
+                            style: TextStyle(
+                                color: Colors.greenAccent, fontSize: 70),
+                          )),
+                        ))
+                  : Container(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            '${deck.displayedMin.toString().padLeft(2, '0')}:${deck.displayedSec.toString().padLeft(2, '0')}',
+                            style: TextStyle(
+                                fontSize: 40, color: Colors.greenAccent),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(bottom: 10),
-                          child: Text(
-                            'REPS: ${deck.displayedCard.reps} / ${deck.deck.length} cards left',
-                            style: TextStyle(color: Colors.green, fontSize: 20),
+                          Padding(
+                            padding: const EdgeInsets.all(20),
+                            child: Image.asset(
+                              deck.displayedCard.imagePath,
+                              scale: 2.0,
+                            ),
                           ),
-                        ),
-                        FlatButton(
-                          onPressed: () {
-                            deck.next();
-                          },
-                          child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.greenAccent,
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Next Card',
-                                  style: TextStyle(fontSize: 25),
-                                ),
-                              )),
-                        )
-                      ],
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 10),
+                            child: Text(
+                              'REPS: ${deck.displayedCard.reps} / ${deck.deck.length} cards left',
+                              style:
+                                  TextStyle(color: Colors.green, fontSize: 20),
+                            ),
+                          ),
+                          FlatButton(
+                            onPressed: () {
+                              deck.next();
+                            },
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: Colors.greenAccent,
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    'Next Card',
+                                    style: TextStyle(fontSize: 25),
+                                  ),
+                                )),
+                          )
+                        ],
+                      ),
                     ),
-                  )),
-                ),
+            ),
+          ),
         );
       },
     );
